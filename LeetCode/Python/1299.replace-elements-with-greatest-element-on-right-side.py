71c2f1b4cd0f3d9181d896b3427498bc3205ce0e
Dipendra-creator <66115431+Dipendra-creator@users.noreply.github.com>
2021-02-14 09:20:37 +0530

    DSA TODO

---
+++ LeetCode/Python/1299.replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,27 @@
+# TODO: Time Limit Exceed in biggest Test case
+# @lc app=leetcode id=1299 lang=python3
+#
+# [1299] Replace Elements with Greatest Element on Right Side
+#
+
+# @lc code=start
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        N = len(arr)
+        l = 0
+
+        for i in range(N):
+            l += 1
+            for j in range(l, N):
+                if arr[i] < arr[j]:
+                    arr[i] = arr[j]
+            if i>0:
+                arr[i-1] = arr[i]
+        
+        arr[N-1] = -1
+        
+        return arr
+        
+        
+# @lc code=end
+
